a71cf65ed0b174498280082976096c318ee38eaf
nyato喵特 <dengquanzhu@gmail.com>
2017-07-12 10:26:03 +0800

    add array

---
+++ ASArray.playground/Contents.swift
@@ -0,0 +1,63 @@
+
+
+extension Sequence {
+    
+    func dqAll(matching predicate: (Iterator.Element) -> Bool) -> Bool {
+        return !contains { !predicate($0) }
+    }
+    
+}
+
+extension Array {
+    func dqFilter(_ isIncluded: (Element) -> Bool) -> [Element] {
+        var resutls = [Element]()
+        
+        for x in self where isIncluded(x) {
+            resutls.append(x)
+        }
+        return resutls
+    }
+    
+    func dqMap<T>(_ condition: (Element) -> T) -> [T] {
+        var results = [T]()
+        
+        for x in self  {
+            results.append(condition(x))
+        }
+        
+        return results
+    }
+    
+    
+    func dqReduce<T>(_ initialValue: T, _ nextPartialResult: (T, Element) -> T) -> T {
+        var result = initialValue
+        for x in self {
+            result = nextPartialResult(result, x)
+        }
+        return result
+    }
+    
+    func dqFlatMap<T>(_ transform: (Element) -> [T]) -> [T] {
+        var results: [T] = []
+        for x in self {
+            results.append(contentsOf: transform(x))
+        }
+        return results
+    }
+    
+}
+
+let fibs = [1, 2, 3, 4, 5]
+
+// ---------------------------
+
+let suits = ["♠", "♥", "♣", "♦"]
+let ranks = ["J","Q","K","A"]
+
+let result = suits.flatMap { suit in
+    ranks.flatMap{ rank in
+        (suit, rank)
+    }
+}
+
+print(result)
