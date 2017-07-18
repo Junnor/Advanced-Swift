44ba17357f93e2145269d5c46ef3878c7664ae3b
nyato喵特 <dengquanzhu@gmail.com>
2017-07-18 09:44:51 +0800

    Complete collection

--- Chapter3/Collection.playground/Contents.swift
+++ Chapter3/Collection.playground/Contents.swift
@@ -2,12 +2,26 @@
 
 protocol DQQueue {
     
-    
     associatedtype Element
     
     mutating func enqueue(_ newElement: Element)
     mutating func dequeue() -> Element?
 
+}
+
+
+protocol DQCollection: Collection, Sequence {
+    
+    associatedtype Index: Comparable
+    
+    var startIndex: Index { get }
+    
+    var endIndex: Index { get }
+    
+    func index(after i: Index) -> Index
+    
+    subscript(position: Index) -> Iterator.Element { get }
+    
 }
 
 // A FIFO queue 
@@ -33,25 +47,30 @@ struct FIFOQueue<Element>: DQQueue {
     
 }
 
+//extension FIFOQueue: DQCollection {
+//    
+//    public var startIndex: Int { return 0 }
+//    public var endIndex: Int { return left.count + right.count }
+//    
+//    
+//    func index(after i: Int) -> Int {
+//        precondition(i < endIndex)
+//        return i + 1
+//    }
+//    
+//    subscript(position: Index) -> Iterator.Element {
+//        precondition((0..<endIndex).contains(position), "Index out of box")
+//        
+//        if position < left.endIndex {
+//            return left[left.count - position - 1]
+//        } else{
+//            return right[position - left.count]
+//        }
+//    }
+//
+//}
 
 
-var fifo = FIFOQueue(left: [""], right: [""])
-fifo.left.removeAll()
-fifo.right.removeAll()
-
-fifo.enqueue("zhu")
-fifo.enqueue("deng")
-var value = fifo.dequeue()
-
-value
-
-
-fifo.enqueue("quan")
-
-value = fifo.dequeue()
-
-
-value
 
 
 
